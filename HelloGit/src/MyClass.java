9bf9a5d474901bead270cf4f425116914f7f757d
김동우 <@DESKTOP-E9CTHB5>
2020-06-17 17:14:02 +0900

    First commit in dev branch

--- HelloGit/src/MyClass.java
+++ HelloGit/src/MyClass.java
@@ -4,6 +4,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("this is 2 ");
+		MyHello myHello = new MyHello();
 		new MyHello().sayHello();
 		
 
